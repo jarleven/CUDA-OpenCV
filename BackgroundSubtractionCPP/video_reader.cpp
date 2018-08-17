3e124743894001c5304c3b61476a3dd80b874a3e
Jarl Even Englund <jarleven@gmail.com>
2018-08-17 21:09:01 +0200

    Make 240x240px images for TensorFlow

--- BackgroundSubtractionCPP/video_reader.cpp
+++ BackgroundSubtractionCPP/video_reader.cpp
@@ -53,9 +53,11 @@ int main(int argc, const char* argv[])
     cv::namedWindow("CPU", cv::WINDOW_NORMAL);
     cv::namedWindow("Mask", cv::WINDOW_NORMAL);
     cv::namedWindow("GPU", cv::WINDOW_OPENGL);
+    cv::namedWindow("CROP", cv::WINDOW_OPENGL);
     cv::cuda::setGlDevice();
 
     cv::Mat frame;
+    cv::Mat orig_frame;
     cv::Mat fgmask;
 
     cv::VideoCapture reader(fname);
@@ -105,6 +107,7 @@ int main(int argc, const char* argv[])
         if (!d_reader->nextFrame(d_frame))
             break;
 
+
         mog2->apply(d_frame, d_fgmask);
 //        mog2->getBackgroundImage(d_bgimg);
 	
@@ -123,7 +126,7 @@ int main(int argc, const char* argv[])
 		counter++;
 		d_frame.download(frame);
                 d_fgmask.download(fgmask);
-
+		d_frame.download(orig_frame);
 
 
 
@@ -169,6 +172,84 @@ int main(int argc, const char* argv[])
 
 			       if((int)radius[i] > 40) {
 				circle( frame, center[i], 2*(int)radius[i], color, 2, 8, 0 );
+
+
+				int x = (int)center[i].x;
+				int y = (int)center[i].y;
+				int xyradius = 2*(int)radius[i];
+
+				int xpos = 0;
+				int ypos = 0;
+				int height = 960;
+				int width = 1280;
+
+				
+      if(xyradius > x) {
+         xpos = 0 ;
+	std::cout << "Left border\n"; 
+
+	}
+
+      else if (xyradius+x > width){
+         xpos = width - (2*xyradius);
+
+	std::cout << "Right border\n"; 
+	
+}
+        else {
+
+		std::cout << "Horisontal center\n"; 
+        	xpos = x-xyradius;
+	}
+        if (xyradius > y){
+         ypos = 0;
+
+		std::cout << "Top border\n"; 
+
+}
+
+        else if (xyradius+y > height) {
+         ypos = height - (2*xyradius);
+		std::cout << "Bottom border\n"; 
+
+}
+        else {
+		std::cout << "Vertical center\n"; 
+
+         ypos = y-xyradius;
+
+	}
+
+        int lengde=2*xyradius;
+
+	cv::Rect rect(xpos, ypos, lengde-1, lengde-1);
+
+	//Mat cropedImage = frame(rect);
+
+	
+		int clen=xpos+lengde;
+		int ch=ypos+lengde;
+				std::cout << xpos << "- " << clen << " -- " << ypos << "-" << ch << "\n"; 
+	
+	if(clen<=width && ch<=height){
+	cv::Rect rect2(600, 600, 200, 200);
+  	cv::Mat croppedImage = orig_frame(rect);
+
+	cv::Size size(240,240);
+	cv::Mat resized;//dst image
+	resize(croppedImage,resized,size);//resize image
+
+	cv::rectangle(frame, rect, cv::Scalar(0, 255, 0));
+
+//        rectangle(frame,(xpos,ypos),(xpos+lengde,ypos+lengde),(0,255,255),2);
+
+			char filenamec[30] = {0};
+	                //(unsigned char const *,int,unsigned char const *,int,bool);
+        	        sprintf(filenamec,"file-%04d_crop%04d.jpg",counter, i);
+	                imwrite(filenamec, resized );
+	
+	}
+
 				}
 			     }
 
