bfb6a5a378e64f5e8ce37d73f18d51d06ea307ed
Jarl Even Englund <jarleven@gmail.com>
2018-08-19 20:47:44 +0200

    Add script to install TensorFlow automatically

---
+++ CUDA8-Tensorflow/install-TensorFlow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt install -y libcupti-dev
+sudo apt install -y python-pip
+pip install --user --upgrade pip
+hash -d pip
+pip install --upgrade pip
+
+pip install tensorflow-gpu==1.4.1
+
+sudo chown -R $USER:$USER /usr/local/lib/python2.7/dist-packages/
+sudo chown -R $USER:$USER /usr/local/bin/
+
+
+pip install --ignore-installed tensorflow-gpu==1.4.1
+
+
+git clone https://github.com/tensorflow/models.git
+cd models/tutorials/image/imagenet
+python classify_image.py
