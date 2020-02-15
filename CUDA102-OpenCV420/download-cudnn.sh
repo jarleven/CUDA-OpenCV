4b56b42c119ef7b2c8adab2e640391d1928bd338
Jarl Even Englund <jarleven@gmail.com>
2020-02-15 22:42:18 +0100

    cuda-10-2.sh

--- CUDA102-OpenCV420/download-cudnn.sh
+++ CUDA102-OpenCV420/download-cudnn.sh
@@ -7,8 +7,8 @@
 # https://developer.nvidia.com/rdp/cudnn-download#a-collapse765-10
 
 cd ~
-# tar -zxf cudnn-10.2-linux-x64-v7.6.5.32.tgz
-tar -zxf  cudnn-10.0-linux-x64-v7.6.5.32.tgz
+tar -zxf cudnn-10.2-linux-x64-v7.6.5.32.tgz
+# tar -zxf  cudnn-10.0-linux-x64-v7.6.5.32.tgz
 cd cuda
 sudo cp -P lib64/* /usr/local/cuda/lib64/
 sudo cp -P include/* /usr/local/cuda/include/
