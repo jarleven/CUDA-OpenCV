67c3dfc78e25d40287d7ecad5b9cf4e16a5472e4
Jarl Even Englund <jarleven@gmail.com>
2020-02-23 16:01:13 +0100

    cuDNN from .deb file

--- CUDA102-OpenCV420/cudnn-10.0.sh
+++ CUDA102-OpenCV420/cudnn-10.0.sh
@@ -9,25 +9,47 @@ source .setupvars
 
 cd ~
 
-while true
-do
+sudo dpkg -i libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb
 
-    if md5sum -c cudnn-10.0.md5; then
-        # The MD5 sum matched
-        echo "OK, found cuDNN files"
+sudo dpkg -i libcudnn7-dev_7.6.5.32-1+cuda10.0_amd64.deb 
 
-        tar -zxf  cudnn-10.0-linux-x64-v7.6.5.32.tgz
-        cd cuda
-        sudo cp -P lib64/* /usr/local/cuda-10.0/lib64/
-        sudo cp -P include/* /usr/local/cuda-10.0/include/
+sudo dpkg -i libcudnn7-doc_7.6.5.32-1+cuda10.0_amd64.deb 
 
-        break
 
-    else
-        echo "cuDNN installer not found, please download from link below and save cudnn-10.2-linux-x64-v7.6.5.32.tgz in home directory "
-        echo "https://developer.nvidia.com/rdp/cudnn-download"
-	sleep 10
-    fi
+cp -r /usr/src/cudnn_samples_v7/ $HOME
+cd  $HOME/cudnn_samples_v7/mnistCUDNN
+make clean && make
+./mnistCUDNN
 
-done
+# TDOD we can test that this command was OK
+
+
+
+
+
+
+
+
+
+#while true
+#do
+#
+#    if md5sum -c cudnn-10.0.md5; then
+#        # The MD5 sum matched
+#        echo "OK, found cuDNN files"
+#
+#        tar -zxf  cudnn-10.0-linux-x64-v7.6.5.32.tgz
+#        cd cuda
+#        sudo cp -P lib64/* /usr/local/cuda-10.0/lib64/
+#        sudo cp -P include/* /usr/local/cuda-10.0/include/
+#
+#        break
+#
+#    else
+#        echo "cuDNN installer not found, please download from link below and save cudnn-10.2-linux-x64-v7.6.5.32.tgz in home directory "
+#        echo "https://developer.nvidia.com/rdp/cudnn-download"
+#	sleep 10
+#    fi
+#
+#done
 
