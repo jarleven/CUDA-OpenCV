4c78aae199c93dc1e9350d5c7c1fa33280883b82
jarleven <jarleven@gmail.com>
2020-03-01 14:19:40 +0100

    Log of version
    On Ubunu 18.04.4

--- CUDA102-OpenCV420/install-nvidia.sh
+++ CUDA102-OpenCV420/install-nvidia.sh
@@ -5,11 +5,12 @@
 # sudo apt install -y nvidia-driver-430
 # When installing CUDA do "sudo apt-get install -y cuda-toolkit-10-0" not just "sudo apt-get install -y cuda"
 
+
 source .setupvars
 
 cd ~
 
-#echo "What drivers are available ?  Just for information."
+echo "What drivers are available ?  Just for information."
 ubuntu-drivers devices
 sleep 5
 
@@ -28,3 +29,25 @@ sudo apt-mark nvidia-driver-$SCRIPT_NVIDIAVER
 sudo apt update
 sudo apt upgrade -y
  
+
+# I REALLY DOUBT THE VERSION IS CONSIDERED WHEN INSTALLING !!!
+# WHEN INSTALLING version 418 !
+
+#
+# +-----------------------------------------------------------------------------+
+# | NVIDIA-SMI 440.59       Driver Version: 440.59       CUDA Version: 10.2     |
+# |-------------------------------+----------------------+----------------------+
+# | GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
+# | Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
+# |===============================+======================+======================|
+# |   0  GeForce GTX 106...  Off  | 00000000:01:00.0  On |                  N/A |
+# |  0%   34C    P8     5W / 120W |    186MiB /  3018MiB |      0%      Default |
+# +-------------------------------+----------------------+----------------------+
+#
+# +-----------------------------------------------------------------------------+
+# | Processes:                                                       GPU Memory |
+# |  GPU       PID   Type   Process name                             Usage      |
+# |=============================================================================|
+# |    0      1080      G   /usr/lib/xorg/Xorg                           113MiB |
+# |    0      1281      G   /usr/bin/gnome-shell                          69MiB |
+# +-----------------------------------------------------------------------------+
