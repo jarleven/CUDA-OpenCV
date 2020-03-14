7c75f64b47a9b11e41e27d21a5f9616849143b77
Jarl Even Englund <jarleven@gmail.com>
2020-03-14 12:50:49 +0100

    Configurable pipeline.config

--- CUDA102-OpenCV420/pipeline_config_massage.sh
+++ CUDA102-OpenCV420/pipeline_config_massage.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-sed -i "/num_classes:/c\    num_classes: 1" pipeline.config
-sed -i "/batch_size:/c\  batch_size: 2" pipeline.config
+NUM_CLASSES=1
+BATCH_SIZE=2
+NUM_STEPS=200000
+
+sed -i "/num_classes:/c\    num_classes: $NUM_CLASSES" pipeline.config
+sed -i "/batch_size:/c\  batch_size: $BATCH_SIZE" pipeline.config
+sed -i "/num_steps:/c\  num_steps: $NUM_STEPS" pipeline.config
+
 sed -i '/train.record/c\  input_path: "annotations\/train.record"' pipeline.config
 sed -i '/val.record/c\  input_path: "annotations\/test.record"' pipeline.config
 sed -i '/label_map.pbtxt/c\  label_map_path: "annotations\/label_map.pbtxt"' pipeline.config
 sed -i 's/PATH_TO_BE_CONFIGURED/pre-trained-model/g' pipeline.config
 
-#sed -i "/fine_tune_checkpoint: "PATH_TO_BE_CONFIGURED\/model.ckpt"/c\fine_tune_checkpoint: "pre-trained-model\/model.ckpt"" pipeline.config
 
 
