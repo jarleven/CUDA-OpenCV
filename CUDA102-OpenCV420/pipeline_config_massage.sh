d4ae6d52802dccb01e41a720ec0ed948f33e44fd
jarleven <jarleven@gmail.com>
2020-03-14 19:27:01 +0100

    Just some text

--- CUDA102-OpenCV420/pipeline_config_massage.sh
+++ CUDA102-OpenCV420/pipeline_config_massage.sh
@@ -4,6 +4,16 @@ NUM_CLASSES=1
 BATCH_SIZE=2
 NUM_STEPS=200000
 
+
+###
+#
+# Most of this work was done on GTX1060 3GB version. 
+# There is only one class in the salmon detector
+# The batch size is restricted by the memory on the GPU. If to large you will get a OOM (Out Of Memory)
+#
+#
+
+
 sed -i "/num_classes:/c\    num_classes: $NUM_CLASSES" pipeline.config
 sed -i "/batch_size:/c\  batch_size: $BATCH_SIZE" pipeline.config
 sed -i "/num_steps:/c\  num_steps: $NUM_STEPS" pipeline.config
