2c16dd0ecc145729ce16817db7f0b28273b84252
Jarl Even Englund <jarleven@gmail.com>
2020-03-15 17:37:03 +0100

    Examples

--- CUDA102-OpenCV420/install-nvidia-docker.sh
+++ CUDA102-OpenCV420/install-nvidia-docker.sh
@@ -20,3 +20,23 @@ curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.li
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
 sudo systemctl restart docker
 
+
+
+exit
+
+# USAGE examples from https://github.com/NVIDIA/nvidia-docker
+
+
+#### Test nvidia-smi with the latest official CUDA image
+docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
+
+# Start a GPU enabled container on two GPUs
+docker run --gpus 2 nvidia/cuda:10.0-base nvidia-smi
+
+# Starting a GPU enabled container on specific GPUs
+docker run --gpus '"device=1,2"' nvidia/cuda:10.0-base nvidia-smi
+docker run --gpus '"device=UUID-ABCDEF,1"' nvidia/cuda:10.0-base nvidia-smi
+
+# Specifying a capability (graphics, compute, ...) for my container
+# Note this is rarely if ever used this way
+docker run --gpus all,capabilities=utility nvidia/cuda:10.0-base nvidia-smi
