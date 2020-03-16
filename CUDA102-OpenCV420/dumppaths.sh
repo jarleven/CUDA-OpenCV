d7d5be9995686e1094a402e60c5e74f2c08e1b13
Jarl Even Englund <jarleven@gmail.com>
2020-03-16 16:19:43 +0100

    Print all paths

--- CUDA102-OpenCV420/dumppaths.sh
+++ CUDA102-OpenCV420/dumppaths.sh
@@ -3,28 +3,32 @@
 
 echo "PATH"
 echo $PATH
-echo -e "\n\n"
+echo -e "\n"
 
 echo "PYTHONPATH"
 echo $PYTHONPATH
-echo -e "\n\n"
+echo -e "\n"
 
 echo "LD_LIBRARY_PATH"
 echo $LD_LIBRARY_PATH
-echo -e "\n\n"
+echo -e "\n"
 
 
 echo "MANPATH"
 echo $MANPATH
-echo -e "\n\n"
+echo -e "\n"
 
 echo "INFOPATH"
 echo $INFOPATH
-echo -e "\n\n"
+echo -e "\n"
 
+echo "CUDA_ROOT"
+echo $CUDA_ROOT
+echo -e "\n"
 
+echo "CUDA_HOME"
+echo $CUDA_HOME
+echo -e "\n"
 
 
 echo -e "\n"
-
-echo "end"
