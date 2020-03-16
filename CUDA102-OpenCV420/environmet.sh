50547940461a4cb6255ac2d37552d840350a77b8
Jarl Even Englund <jarleven@gmail.com>
2020-03-16 15:52:02 +0100

    Environment from other setup files

--- CUDA102-OpenCV420/environmet.sh
+++ CUDA102-OpenCV420/environmet.sh
@@ -5,28 +5,6 @@
 
 #CUDAVERSION
 
-
-
-#export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-#export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}
-#export LD_LIBRARY_PATH=/usr/local/cuda-10.0/extras/CUPTI/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-
-
-#echo 'export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research/object_detection:$HOME/TensorFlow/models/research:$HOME/TensorFlow/models/research/slim"' >> ~/.bash_profile
-
-# 
-# "export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim"     is done above
-# Print-working-directory pwd should be done from within the TensorFlow/models/research/ folder according the guides.
-
-#echo 'export PATH="${PATH:+${PATH}:}/home/linuxbrew/.linuxbrew/bin"' >> ~/.bash_profile
-
-#echo 'export MANPATH="${MANPATH:+${MANPATH}:}/home/linuxbrew/.linuxbrew/share/man"' >> ~/.bash_profile
-
-#echo 'export INFOPATH="${INFOPATH:+${INFOPATH}:}/home/linuxbrew/.linuxbrew/share/info"' >> ~/.bash_profile
-
-
-
-
 if test ${OPENCV_ENV_EXPORTED+defined};
 then
 	echo "Variable OPENCV_ENV_EXPORTED exists!"
@@ -42,7 +20,7 @@ else
 
 	export PATH="${PATH:+${PATH}:}/usr/local/cuda-10.2/bin"
         export PATH="${PATH:+${PATH}:}$HOME/.local/bin"
-
+	export PATH="${PATH:+${PATH}:}/home/linuxbrew/.linuxbrew/bin"
 
 	export CUDA_ROOT="${CUDA_ROOT:+${CUDA_ROOT}:}/usr/local/cuda-10.2"
 	export CUDA_HOME="${CUDA_HOME:+${CUDA_HOME}:}/usr/local/cuda-10.2"
@@ -59,6 +37,10 @@ else
 	export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research/object_detection"
 	export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research"
 	export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research/slim"
+
+
+	export MANPATH="${MANPATH:+${MANPATH}:}/home/linuxbrew/.linuxbrew/share/man"
+	export INFOPATH="${INFOPATH:+${INFOPATH}:}/home/linuxbrew/.linuxbrew/share/info"
 
 fi
 
