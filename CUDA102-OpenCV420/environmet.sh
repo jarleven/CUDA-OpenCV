ca44baaab3ee163225650522e0f2571c68351777
jarleven <jarleven@gmail.com>
2020-03-16 14:43:03 +0100

    Only run once

--- CUDA102-OpenCV420/environmet.sh
+++ CUDA102-OpenCV420/environmet.sh
@@ -10,25 +10,38 @@
 
 #CUDAVERSION
 
-#
-# export TEST="${TEST:+${TEST}:}/A/B/C"
-#
-
-export PATH="${PATH:+${PATH}:}/usr/local/cuda-10.2/bin"
-
-export CUDA_ROOT="${CUDA_ROOT:+${CUDA_ROOT}:}/usr/local/cuda-10.2"
-export CUDA_HOME="${CUDA_HOME:+${CUDA_HOME}:}/usr/local/cuda-10.2"
-
-export PATH="${PATH:+${PATH}:}/usr/local/cuda-10.2/bin"
 
 
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+${LD_LIBRARY_PATH}:}/usr/local/cuda-10.2/lib64"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+${LD_LIBRARY_PATH}:}/usr/local/cuda/extras/CUPTI/lib64"
+if test ${OPENCV_ENV_EXPORTED+defined};
+then
+	echo "Variable somevar exists!"
+else
+	echo "Variable somevar does not exist!"
+	export OPENCV_ENV_EXPORTED="TRUE"
+
+	#
+	# export TEST="${TEST:+${TEST}:}/A/B/C"
+	#
+
+	export PATH="${PATH:+${PATH}:}/usr/local/cuda-10.2/bin"
+
+	export CUDA_ROOT="${CUDA_ROOT:+${CUDA_ROOT}:}/usr/local/cuda-10.2"
+	export CUDA_HOME="${CUDA_HOME:+${CUDA_HOME}:}/usr/local/cuda-10.2"
+
+	export PATH="${PATH:+${PATH}:}/usr/local/cuda-10.2/bin"
+
+
+	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+${LD_LIBRARY_PATH}:}/usr/local/cuda-10.2/lib64"
+	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+${LD_LIBRARY_PATH}:}/usr/local/cuda/extras/CUPTI/lib64"
 
 
 
-export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research/object_detection"
-export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research"
-export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research/slim"
+	export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research/object_detection"
+	export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research"
+	export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$HOME/TensorFlow/models/research/slim"
+
+fi
 
 
+#MANPATH
+#INFOPATH
