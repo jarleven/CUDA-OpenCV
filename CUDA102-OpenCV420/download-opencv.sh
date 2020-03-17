3eafb7f2b88be354cc7059198d88558610fee9c0
jarleven <jarleven@gmail.com>
2020-03-17 16:01:50 +0100

    Update download-opencv.sh
    Configurable version

--- CUDA102-OpenCV420/download-opencv.sh
+++ CUDA102-OpenCV420/download-opencv.sh
@@ -2,15 +2,17 @@
 
 # Download OpenCV
 
+#OPENCV_VER="4.2.0"
+OPENCV_VER="3.4.9"
 
 cd ~
 
 if md5sum -c opencv.md5; then
     echo "OpenCV already downloaded"
 else
-    wget -O opencv.zip https://github.com/opencv/opencv/archive/4.2.0.zip
-    wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.2.0.zip
-    wget -O opencv_extra.zip https://github.com/opencv/opencv_extra/archive/4.2.0.zip
+    wget -O opencv.zip https://github.com/opencv/opencv/archive/$OPENCV_VER.zip
+    wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/$OPENCV_VER.zip
+    wget -O opencv_extra.zip https://github.com/opencv/opencv_extra/archive/$OPENCV_VER.zip
 fi
 
 # Unpack OpenCV
@@ -19,8 +21,8 @@ unzip opencv.zip
 unzip opencv_contrib.zip
 unzip opencv_extra.zip
 
-mv opencv-4.2.0 opencv
-mv opencv_contrib-4.2.0 opencv_contrib
-mv opencv_extra-4.2.0 opencv_extra
+mv opencv-$OPENCV_VER opencv
+mv opencv_contrib-$OPENCV_VER opencv_contrib
+mv opencv_extra-$OPENCV_VER opencv_extra
 
 
