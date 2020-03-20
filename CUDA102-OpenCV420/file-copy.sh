d6babf02dd9654fdac2b6169b91390673ef54141
Jarl Even Englund <jarleven@gmail.com>
2020-03-20 23:36:49 +0100

    Error checking

--- CUDA102-OpenCV420/file-copy.sh
+++ CUDA102-OpenCV420/file-copy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 
-set -e  # Exit immediately if a command exits with a non-zero status. (Exit on error)
+set -e  # Exit immediately if a command exits with a non-zero status. Exit on error
 set -x  # Print commands and their arguments as they are executed.
 set -u  # Treat unset variables as an error when substituting.
 
 
-
-cp /media/$USER/CUDA/CUDAFILES/Video_Codec_SDK_9.1.23.zip
-
+cp /media/$USER/CUDA/CUDAFILES/Video_Codec_SDK_9.1.23.zip ~/
 
--- CUDA102-OpenCV420/preparation.sh
+++ CUDA102-OpenCV420/preparation.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-#####################################################
-#
-# Testing .deb files as suggested by Tensorflow
-#
-####
-#
-# # Copy all the md5 files so we can check them with ease later
-# cp -v md5/*.md5 ~/
-# 
-# echo "Copy files form USB"
-# cp -v /media/jarleven/CUDA/CUDAFILES/* ~/
-#
-#
 
-
-set -e  # Exit immediately if a command exits with a non-zero status. (Exit on error)
+set -e  # Exit immediately if a command exits with a non-zero status. Exit on error
 set -x  # Print commands and their arguments as they are executed.
 set -u  # Treat unset variables as an error when substituting.
 
 
-
-cp /media/$USER/CUDA/CUDAFILES/Video_Codec_SDK_9.1.23.zip
 
 
 
