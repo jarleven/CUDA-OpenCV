41cb3f13069ef21c9b22c379eb8e6287133a33c4
Jarl Even Englund <jarleven@gmail.com>
2020-03-21 00:28:12 +0100

    Failiure during autoinstall

--- CUDA102-OpenCV420/statemachine.sh
+++ CUDA102-OpenCV420/statemachine.sh
@@ -1,13 +1,17 @@
-#/bin/bash
+#!/bin/bash
 
 set -e  # Exit immediately if a command exits with a non-zero status. Exit on error
 set -x  # Print commands and their arguments as they are executed.
 set -u  # Treat unset variables as an error when substituting.
 
+sleep 5
+
+pwd
 cd "$(dirname "$0")"
+pwd
 
 
-source ./statemachine.state
+source statemachine.state
 
 while :
 do
