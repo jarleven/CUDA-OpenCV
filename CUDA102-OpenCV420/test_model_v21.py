987ce714ace81ec41a6f74a53671743602732697
Jarl Even Englund <jarleven@gmail.com>
2020-06-14 17:04:17 +0200

    Fix for RTX2070 memory issue

--- CUDA102-OpenCV420/test_model_v21.py
+++ CUDA102-OpenCV420/test_model_v21.py
@@ -1,5 +1,14 @@
 import numpy as np
 import tensorflow as tf
+
+# For tensorflow/stream_executor/cuda/cuda_dnn.cc:329] Could not create cudnn handle: CUDNN_STATUS_INTERNAL_ERROR
+# Problem with RTX2070 GPU
+from tensorflow.compat.v1 import InteractiveSession
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+# End RTX2070
+
 import cv2 as cv
 from timeit import default_timer as timer
 
