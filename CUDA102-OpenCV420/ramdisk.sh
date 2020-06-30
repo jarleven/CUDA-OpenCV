4be580396e101b67717c9a92ccdfaa6e205ac7d4
Jarl Even Englund <jarleven@gmail.com>
2020-06-30 12:51:32 +0200

    Use 30% of RAM

--- CUDA102-OpenCV420/ramdisk.sh
+++ CUDA102-OpenCV420/ramdisk.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+DISKSIZE=4G
+DISKSIZE=$(grep MemTotal /proc/meminfo |  awk '{printf "%.0fG", $2=0.3*$2/1024^2}')
+
 
 sudo mkdir -p /tmp/ramdisk
 sudo chmod 777 /tmp/ramdisk
-sudo mount -t tmpfs -o size=4G myramdisk /tmp/ramdisk
+sudo mount -t tmpfs -o size=$DISKSIZE myramdisk /tmp/ramdisk
 mkdir -p /tmp/ramdisk/full
 
