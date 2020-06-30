e877c1247c8b841c63505fe9a684a883fc5989dc
Jarl Even Englund <jarleven@gmail.com>
2020-06-30 13:23:13 +0200

    Trying to fix the Tensorflow unattended build again....

--- CUDA102-OpenCV420/build-tensorflow-21.sh
+++ CUDA102-OpenCV420/build-tensorflow-21.sh
@@ -3,6 +3,12 @@
 # Description
 # https://gist.github.com/kmhofmann/e368a2ebba05f807fa1a90b3bf9a1e03/e79e25b3a847abe69c392d8ed356e58dc879234a
 
+set -e  # Exit immediately if a command exits with a non-zero status. (Exit on error)
+set -x  # Print commands and their arguments as they are executed.
+set -u  # Treat unset variables as an error when substituting.
+
+
+cd ~
 sudo apt install -y python3-dev python3-pip
 pip3 install -U --user pip six numpy wheel setuptools mock 'future>=0.17.1'
 pip3 install -U --user keras_applications --no-deps
@@ -18,10 +24,12 @@ wget https://github.com/bazelbuild/bazel/releases/download/0.29.1/bazel-0.29.1-l
 sudo rm -rf /usr/bin/bazel
 sudo mv bazel-0.29.1-linux-x86_64 /usr/bin/bazel
 sudo chmod +x /usr/bin/bazel
-i
+
+
 #If you need to test the installed Bazel version run :
 # which bazel
 # bazel --version
+
 
 # Clone the Tensorflow repository
 cd ~ 
@@ -33,6 +41,13 @@ git checkout v2.1.0
 # TODO if this is to be generic it should handle different versions of CUDA !
 source ~/CUDA-OpenCV/CUDA102-OpenCV420/environmet.sh
 
+
+bazel build --config=opt --python_path=/usr/bin/python3 --config=cuda --noincompatible_strict_action_env //tensorflow/tools/pip_package:build_pip_package --noincompatible_do_not_split_linking_cmdline
+
+#TODO install
+
+
+exit
 
 # --noincompatible_do_not_split_linking_cmdline   FOR THE CuDNN issue
 
