bb369dcd2377eb24a6bdbb3c889f0d0241624cb4
Jarl Even Englund <jarleven@gmail.com>
2020-07-04 13:06:43 +0200

    Utils for concatenating and overlaying video sequences

---
+++ CUDA102-OpenCV420/install-video-create-tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+PLAYSEQUENCE
+---------------------------
+  Following was in GPU4 home
+    playsequence
+    pythonclock
+
+
+Imagemagic ??
+
+sudo apt install inkscape
+sudo add-apt-repository -y ppa:inkscape.dev/stable
+sudo apr update
+sudo apt install -y inkscape
+  
