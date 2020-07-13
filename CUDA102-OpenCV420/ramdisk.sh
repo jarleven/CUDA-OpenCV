764c1b598a1a2c4f5f9d25b12e0a0b9b224f8428
Jarl Even Englund <jarleven@gmail.com>
2020-07-13 23:17:42 +0200

    Create directory for logging

--- CUDA102-OpenCV420/ramdisk.sh
+++ CUDA102-OpenCV420/ramdisk.sh
@@ -7,5 +7,6 @@ DISKSIZE=$(grep MemTotal /proc/meminfo |  awk '{printf "%.0fG", $2=0.3*$2/1024^2
 sudo mkdir -p /tmp/ramdisk
 sudo chmod 777 /tmp/ramdisk
 sudo mount -t tmpfs -o size=$DISKSIZE myramdisk /tmp/ramdisk
-mkdir -p /tmp/ramdisk/full
 
+mkdir -p /tmp/ramdisk/full
+mkdir -p /tmp/ramdisk/log
